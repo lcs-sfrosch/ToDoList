c8d4ca52a6520cb3865b8f8cd1ffa0df3caedc40
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-04-13 09:03:57 -0400

    fixed alll the syntax errors

--- ToDoList/Views/ListView.swift
+++ ToDoList/Views/ListView.swift
@@ -1,69 +1,51 @@
-//
-//  ListView .swift
-//  ToDoList
-//
-//  Created by Samantha Stewart on 2023-04-03.
-//
-
 import SwiftUI
+import Blackbird
 
 struct ListView: View {
-    @Environment(\.blackbirdDatabase) var db:
-    Blackbird.Database?
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
     
     @BlackbirdLiveModels({ db in
         try await TodoItem.read(from: db)
-    }) var todoItems
-    @State var todoItems: [TodoItem] = existingTodoItems
+    }) var liveTodoItems
+    
+    @State var todoItems: [TodoItem] = []
     @State var newItemDescription: String = ""
     
     var body: some View {
         NavigationView {
             VStack {
                 HStack {
-                    TextField("Enter a to-do item", text:
-                                Binding.constant (""))
+                    TextField("Enter a to-do item", text: $newItemDescription)
                     Button(action: {
-                        let lastId = todoItems.last!.id
-                        
+                        let lastId = todoItems.last?.id ?? 0
                         let newId = lastId + 1
                         
-                        let newTodoItem = TodoItem(id: newId,
-                                                   description:newItemDescription, completed: false)
+                        let newTodoItem = TodoItem(id: newId, description: newItemDescription, completed: false)
                         todoItems.append(newTodoItem)
                         newItemDescription = ""
-                        
                     }, label: {
-                        Text ("ADD")
-                            .font (.caption)
+                        Text("ADD")
+                            .font(.caption)
                     })
-                    
-                    
-                    
                 }
                 .padding(20)
                 
-                List(existingTodoItems) { currentItem in
+                List(todoItems) { currentItem in
                     Label(title: {
                         Text(currentItem.description)
                     }, icon: {
-                        if currentItem.completed == true {
+                        if currentItem.completed {
                             Image(systemName: "checkmark.circle")
                         } else {
                             Image(systemName: "circle")
                         }
                     })
-                
-                    
-                    
                 }
-                
             }
+            .navigationTitle("Todo List")
         }
-        
     }
 }
-            
 
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
